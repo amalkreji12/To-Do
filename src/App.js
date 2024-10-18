c3d18fcb392f2b8f49e7f3cef4cfa3c17f2a38b4
Amal K Reji <amalreji45.amal@gmail.com>
2024-10-18 15:51:10 +0530

    added delete

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
 
       <div className="input">
         <input value={toDo} onChange={(e) => setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={() => setTodos([...toDos,{id:Date.now(),text:toDo,status:false}])} className="fas fa-plus"></i>
+        <i onClick={() => setTodos([...toDos, { id: Date.now(), text: toDo, status: false }])} className="fas fa-plus"></i>
       </div>
 
       <div className="todos">
@@ -30,47 +30,42 @@ function App() {
           return (
             <div className="todo">
               <div className="left">
-                <input onChange={(e)=>{
+                <input onChange={(e) => {
                   console.log(e.target.checked);
                   console.log(obj);
-                  setTodos(toDos.filter(obj2=>{
-                    if(obj2.id===obj.id){
-                      obj2.status=e.target.checked;
+                  setTodos(toDos.filter(obj2 => {
+                    if (obj2.id === obj.id) {
+                      obj2.status = e.target.checked;
                     }
                     return obj2;
                   }))
-                  
-                }} 
-                value={obj.status} type="checkbox" name="" id="" />
+
+                }}
+                  value={obj.status} type="checkbox" name="" id="" />
                 <p>{obj.text}</p>
-                
+
               </div>
               <div className="right">
-                <i className="fas fa-times"></i>
+                <i onClick={() => setTodos(toDos.filter((obj2) => obj2.id !== obj.id))} className="fas fa-times"></i>
               </div>
             </div>
           )
         })}
 
-        {toDos.map((obj) =>{
-          if(obj.status){
-            return(<h1>{obj.text}</h1>
+        <h1>Completed Task :</h1>
+        {toDos.map((obj) => {
+          if (obj.status) {
+            return (<h1>{obj.text}</h1>
             )
             return null
           }
         })}
 
-
       </div>
 
     </div>
 
-
-
   );
-
-
-
 
 }
 
